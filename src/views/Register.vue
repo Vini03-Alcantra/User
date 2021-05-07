e69e2faaca2f805649147ca6acb7d2d1bb3e233e
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-07 16:29:40 -0300

    COmiit inicial

---
+++ src/views/Register.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Registro de Usuário</h2>
+        <hr>
+        
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <p>Name</p>
+                <input type="text" placeholder="Name of user" class="input">
+                <p>E-mail</p>
+                <input type="email" placeholder="email@example.com" class="input">
+                <p>Senha</p>
+                <input type="password" placeholder="******" class="input">
+                <hr>
+                <button class="button is-success">Cadastrar</button>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export  default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>

0eba33fd9600642289a28a28276bc7b7c3485b2b
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-07 23:19:08 -0300

    Criando tela de login

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <hr>
+
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <div v-if="error != undefined">
+                    <div class="notification is-danger">
+                        <p>{{error}}</p>
+                    </div>
+                </div>
+                <p>Email</p>
+                <input type="email" placeholder="email@example.com.br" class="input" v-model="email">
+                <p>Password</p>
+                <input type="password" placeholder="********" class="input" v-model="password">
+                <hr>
+                <button class="button is-success" @click="login">Login</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {            
+            password: "",
+            email: "",
+            error: undefined
+        }
+    },
+    methods:{
+        login(){
+            console.log("Login")
+        }
+    }
+}
+
+</script>

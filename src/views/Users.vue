836cd50625dd4c51f0da9c037ede5435454f278c
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-10 22:16:42 -0300

    criando modal

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -20,11 +20,34 @@
                     <td>{{user.role | processRole}}</td>
                     <td>
                         <button class="button is-success">Editar</button> | 
-                        <button class="button is-danger">Deletar</button>    
+                        <button class="button is-danger" @click="showModalUser(user.id)">Deletar</button>    
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <div :class="{modal: true, 'is-active': showModal}">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Você quer realmente deleter esse usuário
+                        </p>                        
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <p>Você está prestes a deletar o usuário</p>
+                        </div>
+                    </div>
+                    <footer class="card-footer">                        
+                        <a href="#" class="card-footer-item" @click="hideModal()">Fechar</a>
+                        <a href="#" class="card-footer-item">Deletar</a>
+                    </footer>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="hideModal()"></button>
+        </div>
     </div>
 </template>
 
@@ -48,7 +71,17 @@ export default{
     },
     data(){
         return {
-            users: []
+            users: [],
+            showModal: false
+        }
+    },
+    methods: {
+        hideModal(){
+            this.showModal = false
+        },
+        showModalUser(id){
+            console.log("Id do user: " +id)
+            this.showModal = true
         }
     },
     filters: {
